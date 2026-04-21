ec32275c5786cd368d4247153117931c35b955fc
Trevor <thoner1@protonmail.com>
2026-04-21 15:41:22 -0400

    added directories

---
+++ configuration.nix
@@ -0,0 +1,66 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "battlestation"; 
+  networking.networkmanager.enable = true;
+
+  #IWD configurations
+  networking.networkmanager.wifi.backend = "iwd";
+  networking.wireless.iwd.enable = true;
+  networking.wireless.enable = false;
+
+  time.timeZone = "America/New_York";
+
+  services.displayManager.ly.enable = true;
+  services.xserver = {
+	enable = true;
+	autoRepeatDelay = 200;
+	autoRepeatInterval = 35;
+	windowManager.qtile.enable = true;
+  };
+
+
+  users.users.trevor = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; 
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  programs.firefox.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    vim 
+    wget
+    git
+    alacritty
+    lutris
+    wireguard-tools
+    wireguard-ui
+    xclip
+    maim
+    bluez
+    xscreensaver
+  ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) 
+  [ "steam" "steam-unwrapped" ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.stateVersion = "25.11"; 
+
+}
+
---
+++ home.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, ... }:
+let
+  dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
+  link = path: config.lib.file.mkOutOfStoreSymlink "${dotfiles}/${path}";
+in
+{
+    xdg.configFile = { 
+      "alacritty" = { source = link "alacritty"; };
+      "nvim"      = { source = link "nvim"; };
+      "qtile"     = { source = link "qtile"; };
+      "rofi"      = { source = link "rofi"; };
+    };
+
+    programs.git = {
+      enable = true;
+      userName = "Trevor";
+      userEmail = "thoner1@protonmail.com";
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
+    };
+
+    home.stateVersion = "25.11";
+    programs.bash = {
+	    enable = true;
+	    shellAliases = { 
+	      btw = "echo I use nixos, btw";
+        nrs = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#battlestation"; 
+        nrs-dry = "sudo nixos-rebuild test --flake ~/nixos-dotfiles#battlestation"; 
+	};
+    };
+    
+    home.packages = with pkgs; [
+    	neovim
+    	ripgrep
+    	nil
+    	nixpkgs-fmt
+    	nodejs
+    	gcc
+      rofi
+      neofetch
+      fastfetch
+      iwd
+      xfce.thunar
+      wine
+      onlyoffice-desktopeditors
+    ];
+}
